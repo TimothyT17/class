e9a18aaa4826934eec1e1ad8d17cd29399c476d2
Timothy Tanuwidjaja <Timothy>
2020-02-27 15:10:48 +0700

    First commit

---
+++ calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  calculator
+//
+//  Created by Timothy Tanuwidjaja on 27/02/20.
+//  Copyright © 2020 Timothy Tanuwidjaja. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
